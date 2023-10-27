b181f0e6d964be36dc749cd1c2b5c3cd998ef42b
Juan Cabello <jcabelloc@itana.pe>
2023-10-27 17:54:12 -0500

    Creacion del Configuration Service y Configuracion Basica para proveer configuracion para Prestamos

---
+++ configuration-service/src/test/java/com/abc/configuration/service/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.configuration.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
