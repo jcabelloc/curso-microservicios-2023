0e822fb1f66f343768f5e814104488eebf143097
Juan Cabello <jcabelloc@itana.pe>
2023-11-03 18:48:12 -0500

    Configurar Fallback y Retry Patterns  en Prestamos

--- prestamos/src/main/java/com/abc/prestamos/client/ClienteClient.java
+++ prestamos/src/main/java/com/abc/prestamos/client/ClienteClient.java
@@ -1,6 +1,7 @@
 package com.abc.prestamos.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -11,11 +12,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(value = "clientes")
 public interface ClienteClient {
+
+
+  Logger logger = LoggerFactory.getLogger(ClienteClient.class);
+
   @RequestMapping(
       method= RequestMethod.GET,
       value="/clientes/{codCliente}",
       consumes="application/json")
-  @CircuitBreaker(name = "clientes")  // <== Corresponde a la instancia Circuit Breaker del Config
+  @CircuitBreaker(name = "clientes", fallbackMethod = "getClienteFallback")  // <== Corresponde a la instancia Circuit Breaker del Config
+  @Retry(name = "clientes")
   ClienteDto getCliente(@PathVariable("codCliente") Integer codCliente);
+
+
+  default ClienteDto getClienteFallback(Integer codCliente, Exception e) {
+    logger.info("Obteniendo al cliente {} mediante fallback", codCliente);
+    return ClienteDto
+        .builder()
+        .codCliente(codCliente)
+        .nombres("--")
+        .apellidoPaterno("--")
+        .apellidoMaterno("--")
+        .clasificacion(0)
+        .build();
+  }
 
 }
