f15ab96422f74201c1dae1ae917f396fccd1dbf5
Juan Cabello <jcabelloc@itana.pe>
2023-11-06 17:54:10 -0500

    Gateway Service con rutas dinamicas basadas en el discovery

--- gateway-service/src/main/java/com/abc/gateway/service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/abc/gateway/service/GatewayServiceApplication.java
@@ -2,8 +2,10 @@ package com.abc.gateway.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope // <== Habilita Endpoint para refrescar la configuracion
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
